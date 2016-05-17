69c49175a7f14a8eb707e4c18dd8954476d9aedb
DebbieRoycroft <DebbieRoycroft@users.noreply.github.com>
2016-05-17 21:00:25 +0100

    Fix error in link example caused by wrong option syntax for jmod Bring windows script in line with latest mac/linux script Tested on Windows10

--- 07_The_linker/linkModules.bat
+++ 07_The_linker/linkModules.bat
@@ -3,7 +3,7 @@ echo Removing any existing greetings.jmod
 if exist greetings.jmod del /F /Q greetings.jmod
 
 echo Creating a module (greetings.jmod) from multiple modules / packages / classes with jlink.
-jmod --create --class-path mods\com.greetings;mods\org.astro greetings.jmod
+jmod create --class-path mods\com.greetings;mods\org.astro greetings.jmod
 
 echo Enlisting the contents of the module (greetings.jmod).
-jmod --list greetings.jmod
+jmod list greetings.jmod
