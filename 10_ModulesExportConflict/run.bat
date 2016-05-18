8cffc7ff3df63aa8fbce7a2dc6f1d5284ce022c0
DebbieRoycroft <DebbieRoycroft@users.noreply.github.com>
2016-05-18 22:39:17 +0100

    Added windows scripts for example 10 Tested on Windows 10

---
+++ 10_ModulesExportConflict/run.bat
@@ -0,0 +1,6 @@
+@echo off
+ 
+echo Running 'com.greetings.Main' from within the mods folder.
+echo Will fail because 'org.astro' and 'org.astro2' packages are exporting the same module 'org.astro'.
+echo 
+java -modulepath mods -m com.greetings/com.greetings.Main
